ee09e686c07fb02b817a424b2ac152a500d6f43d
kris <hithard5@gmail.com>
2022-07-13 18:53:27 +0200

    Add ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ['./*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        ram: 'repeat(auto-fit, minmax(0, 350px));',
+      },
+
+      animation: {
+        'slide-from-top': 'slide-from-top 0.6s ease-in-out forwards',
+      },
+
+      keyframes: {
+        'slide-from-top': {
+          '100%': { transform: 'translateY(0px)', opacity: 1 },
+        },
+      },
+    },
   },
   plugins: [],
 };
